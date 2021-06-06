6151072f35d065ba532bb285c66c6edc9e24f1dc
S HARSHITH SALANKE <harshithsalanke07@gmail.com>
2021-06-06 20:52:17 +0530

    SortingTest

--- src/test/kotlin/sorting/QuickSortTest.kt
+++ src/test/kotlin/sorting/QuickSortTest.kt
@@ -13,6 +13,7 @@ class QuickSortTest : StringSpec({
             row(intArrayOf(1,2,3,4), intArrayOf(1,2,3,4)),
             row(intArrayOf(4,3,2,1), intArrayOf(1,2,3,4)),
             row(intArrayOf(1,1,2,2), intArrayOf(1,1,2,2)),
+            row(intArrayOf(),intArrayOf()),
             row(intArrayOf(), intArrayOf())
         ) { arr, result ->
             Quick_sort.quicksort(arr) shouldBe result
